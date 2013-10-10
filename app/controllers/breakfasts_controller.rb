232ac433a01eef98e8e4d8e52a58cdcfdd4173b9
Scott Norvell <scottlnorvell@gmail.com>
2013-10-10 09:34:44 -0400

    modify stuff

--- app/controllers/breakfasts_controller.rb
+++ app/controllers/breakfasts_controller.rb
@@ -1,22 +1,30 @@
 class BreakfastsController < ApplicationController
   def index
-  end
 
-  def create
-  end
-
-  def update
   end
 
   def show
-  end
 
-  def destroy
   end
 
   def new
+
+  end
+
+  def create
+
   end
 
   def edit
+
   end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+  
 end
