eb1042a0a3a86802dea4e413efd629902c93ccd2
Scott Norvell <scottlnorvell@gmail.com>
2013-10-11 10:01:26 -0400

    expand scope of seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,21 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-date = '2011-03-21'
+Breakfast.delete_all
 
-url = "http://food.cs50.net/api/1.3/menus?meal=BREAKFAST&sdt=#{date}&output=json"
+['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11','12'].each do |month|
+	(10..31).each do |day|
+		date = "2011-#{month}-#{day}"
 
-(10..31).each do |day|
-	date = "2011-03-#{day}"
+		url = "http://food.cs50.net/api/1.3/menus?meal=BREAKFAST&sdt=#{date}&output=json"
 
-	url = "http://food.cs50.net/api/1.3/menus?meal=BREAKFAST&sdt=#{date}&output=json"
+		resp = HTTParty.get(url)
 
-	resp = HTTParty.get(url)
-	
-	unless resp.empty?
-		resp.each do |breakfast| 
-			Breakfast.create entree: breakfast['name'], day: breakfast['date']
+		unless resp.empty?
+			resp.each do |breakfast| 
+				Breakfast.create entree: breakfast['name'], day: breakfast['date']
+			end
 		end
+		
 	end
-	
 end
-
