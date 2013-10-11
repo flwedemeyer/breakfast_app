f3bf22078ff84ddec63b88733c2453676eed1325
Scott Norvell <scottlnorvell@gmail.com>
2013-10-11 09:53:16 -0400

    seeds and heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+date = '2011-03-21'
+
+url = "http://food.cs50.net/api/1.3/menus?meal=BREAKFAST&sdt=#{date}&output=json"
+
+(10..31).each do |day|
+	date = "2011-03-#{day}"
+
+	url = "http://food.cs50.net/api/1.3/menus?meal=BREAKFAST&sdt=#{date}&output=json"
+
+	resp = HTTParty.get(url)
+	
+	unless resp.empty?
+		resp.each do |breakfast| 
+			Breakfast.create entree: breakfast['name'], day: breakfast['date']
+		end
+	end
+	
+end
+
